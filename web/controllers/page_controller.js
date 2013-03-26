18824adee486f60fe536d3524fe98759f61b35e6
Ben Livermore <benjamin.livermore@cengage.com>
2013-03-26 17:32:34 -0400

    add tabs

--- web/controllers/page_controller.js
+++ web/controllers/page_controller.js
@@ -1,5 +1,21 @@
 steal('jquery/controller', 'jquery/view', 'jquery/view/ejs', function () {
 
+    $.Controller('TabController', {
+        init: function () {
+            
+        },
+
+        '.debateTab click': function () {
+            this.element.find('#debate').show();
+            this.element.find('#task').hide();
+        },
+
+        '.taskTab click': function () {
+            this.element.find('#task').show();
+            this.element.find('#debate').hide();
+        }
+    });
+
     $.Controller('TaskController', {
         init: function () {
             this.element.html('views/start.ejs', {});
--- web/main.js
+++ web/main.js
@@ -1,3 +1,4 @@
 steal('vdebate/controllers/page_controller.js', function () {
     var pageController = new TaskController($('#task'), {});
+    var tabController = new TabController($('#content'), {});
 });
