6cac9b9225218f01aadc3f12e69996086397dcfa
Ben Livermore <benjamin.livermore@cengage.com>
2013-03-26 16:39:40 -0400

    add write ejs

--- web/controllers/page_controller.js
+++ web/controllers/page_controller.js
@@ -5,9 +5,8 @@ steal('jquery/controller', 'jquery/view', 'jquery/view/ejs', function () {
             this.element.html('views/start.ejs', {});
         },
 
-        '.show click': function () {
-            
-            this.element.html('views/start.ejs', {});
+        '.start click': function () {
+            this.element.html('views/write.ejs', {name:"Write opening argument"});
         }
     });
 });
